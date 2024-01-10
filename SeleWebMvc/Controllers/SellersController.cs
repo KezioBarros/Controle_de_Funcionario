776e31ac8b3219a59424e0569a40229fd4be5517
Kezio Pereira Barros <keziopb@gmail.com>
2024-01-10 14:41:50 -0300

    Simple Create form

--- SeleWebMvc/Controllers/SellersController.cs
+++ SeleWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SeleWebMvc.Models;
 using SeleWebMvc.Service;
 
 namespace SeleWebMvc.Controllers
@@ -16,6 +17,19 @@ namespace SeleWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Inset(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
