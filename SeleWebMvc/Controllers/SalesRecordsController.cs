e3576c1cfe0eef5c2b659c9a888dff4f3d722f13
Kezio Pereira Barros <keziopb@gmail.com>
2024-01-18 14:00:20 -0300

    Implementing grouping search

--- SeleWebMvc/Controllers/SalesRecordsController.cs
+++ SeleWebMvc/Controllers/SalesRecordsController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SeleWebMvc.Service;
 
 namespace SeleWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
-        private readonly SalesRecordsController
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,7 +19,18 @@ namespace SeleWebMvc.Controllers
 
         public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if(!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
         public IActionResult GroupingSearch()
